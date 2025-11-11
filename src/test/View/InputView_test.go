f00f32b162d40872cbcd8c3564e2b411180beb33
patkrdj <taejun154@gmail.com>
2025-11-11 15:20:51 +0900

    test(InputView_test.go): 유닛 테스트 코드 작성
    입력값이 정상적인 경우, 음수인 경우, 빈 문자열이 들어오는 경우, 숫자가 아닌 값이 들어오는 경우를 테스트하였음.

--- src/test/View/InputView_test.go
+++ src/test/View/InputView_test.go
@@ -1,10 +1,61 @@
 package View
 
-import "testing"
+import (
+	"Blackjack/src/main/View"
+	"bufio"
+	"strings"
+	"testing"
+)
 
-func TestAdd(t *testing.T) {
-	result := 2 + 5
-	if result != 5 {
-		t.Errorf("Expected 5, got %d", result)
+func Test_ReadString_NormalInput(t *testing.T) {
+	fakeInput := "400\n"
+	inputView := &View.InputView{Reader: bufio.NewReader(strings.NewReader(fakeInput))}
+
+	value, err := inputView.ReadString()
+
+	if err != nil {
+		t.Errorf("예상치 않은 에러 발생")
+	}
+	if value != 400 {
+		t.Errorf("결과값이 다름")
+	}
+}
+
+func Test_Exception_Negative(t *testing.T) {
+	fakeInput := "-122\n"
+	inputView := &View.InputView{Reader: bufio.NewReader(strings.NewReader(fakeInput))}
+
+	_, err := inputView.ReadString()
+
+	if err == nil {
+		t.Errorf("예상치 않은 에러 발생: %v", err)
+	} else {
+		t.Logf("에러가 정상적으로 동작")
+	}
+}
+
+func Test_Exception_EmptyString(t *testing.T) {
+	fakeInput := "\n"
+	inputView := &View.InputView{Reader: bufio.NewReader(strings.NewReader(fakeInput))}
+
+	_, err := inputView.ReadString()
+
+	if err == nil {
+		t.Errorf("예상치 않은 에러 발생: %v", err)
+	} else {
+		t.Logf("에러가 정상적으로 동작")
+	}
+}
+
+func Test_Exception_NotDigit(t *testing.T) {
+	fakeInput := "good\n"
+	inputView := &View.InputView{Reader: bufio.NewReader(strings.NewReader(fakeInput))}
+
+	_, err := inputView.ReadString()
+
+	if err == nil {
+		t.Errorf("예상치 않은 에러 발생: %v", err)
+	} else {
+		t.Logf("에러가 정상적으로 동작")
 	}
 }
