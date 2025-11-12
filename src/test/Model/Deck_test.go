f646be275cef3cc23fbc1a01fbe485fba65aa1c8
patkrdj <taejun154@gmail.com>
2025-11-12 23:33:51 +0900

    test(Deck_test.go): 덱의 draw의 정상 테스트케이스와 예외 테이스케이스 추가

--- src/test/Model/Deck_test.go
+++ src/test/Model/Deck_test.go
@@ -1,1 +1,28 @@
 package Model
+
+import (
+	"Blackjack/src/main/Model"
+	"testing"
+)
+
+func Test_Draw(t *testing.T) {
+	deck := Model.NewDeck()
+	card, err := deck.DrawCard()
+	if err != nil {
+		t.Error(err)
+	}
+	if card != Model.NewCard("♠", "A") {
+		t.Errorf("덱의 제일 첫 장이 ♠A가 아님")
+	}
+}
+
+func Test_Empty_Draw(t *testing.T) {
+	deck := Model.Deck{}
+	_, err := deck.DrawCard()
+	if err == nil {
+		t.Errorf("예상치 않은 에러 발생: %v", err)
+	}
+	if err != nil {
+		t.Logf("에러가 정상적으로 동작")
+	}
+}
