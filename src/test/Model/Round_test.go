9786f5dae40b7cfade912220150e8e0380a44f90
patkrdj <taejun154@gmail.com>
2025-11-13 16:04:48 +0900

    test(Round_test.go): DealInit 메소드를 검사하는 테스트케이스 작성
    정상적으로 동작하는 경우와 dealInit하기에 카드 개수가 부족한 경우를 테스트하였음.

--- src/test/Model/Round_test.go
+++ src/test/Model/Round_test.go
@@ -1,1 +1,59 @@
 package Model
+
+import (
+	"Blackjack/src/main/Model"
+	"testing"
+)
+
+func Test_DealInit(t *testing.T) {
+	cards := make([]Model.Card, 0)
+	cards = append(cards, Model.NewCard("♠", "A"))
+	cards = append(cards, Model.NewCard("♠", "2"))
+	cards = append(cards, Model.NewCard("♠", "3"))
+	cards = append(cards, Model.NewCard("♠", "4"))
+
+	round := Model.NewRound(Model.NewDeck(cards), 1000)
+	err := round.DealInit()
+	if err != nil {
+		t.Error(err)
+	}
+
+	dealer := round.GetDealerHand()
+	player := round.GetPlayerHand()
+	combine := append(dealer.GetCards(), player.GetCards()...)
+
+	if len(combine) != len(cards) {
+		t.Errorf("카드 개수가 다릅니다.")
+	}
+
+	for _, card := range cards {
+		if contains(combine, card) == false {
+			t.Errorf("전체 카드와 뽑은 카드가 다릅니다.")
+		}
+	}
+}
+
+func Test_DealInit_Input_Count_Exception(t *testing.T) {
+	cards := make([]Model.Card, 0)
+	cards = append(cards, Model.NewCard("♠", "A"))
+	cards = append(cards, Model.NewCard("♠", "2"))
+	cards = append(cards, Model.NewCard("♠", "3"))
+
+	round := Model.NewRound(Model.NewDeck(cards), 1000)
+	err := round.DealInit()
+	if err == nil {
+		t.Errorf("예상치 않은 에러 발생: %v", err)
+	}
+	if err != nil {
+		t.Logf("에러가 정상적으로 동작")
+	}
+}
+
+func contains(cards []Model.Card, target Model.Card) bool {
+	for _, card := range cards {
+		if card == target {
+			return true
+		}
+	}
+	return false
+}
