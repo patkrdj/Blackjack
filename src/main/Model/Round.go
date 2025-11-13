97f3bbb84939f853e445efd1e1b85708bbb3265a
patkrdj <taejun154@gmail.com>
2025-11-13 16:06:09 +0900

    feat(Round.go): 게임 라운드를 구현
    초기 카드 배분 구현

--- src/main/Model/Round.go
+++ src/main/Model/Round.go
@@ -1,1 +1,52 @@
 package Model
+
+type Round struct {
+	deck       *Deck
+	dealerHand *Hand
+	playerHand *Hand
+	bet        int
+}
+
+func NewRound(deck *Deck, bet int) *Round {
+	deck.ShuffleDeck()
+	dealerHand := NewHand("dealer")
+	playerHand := NewHand("player")
+	return &Round{deck, dealerHand, playerHand, bet}
+}
+
+func (round *Round) DealInit() error {
+	err := round.dealCard(round.playerHand)
+	if err != nil {
+		return err
+	}
+	err = round.dealCard(round.playerHand)
+	if err != nil {
+		return err
+	}
+	err = round.dealCard(round.dealerHand)
+	if err != nil {
+		return err
+	}
+	err = round.dealCard(round.dealerHand)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (round *Round) dealCard(hand *Hand) error {
+	card, err := round.deck.DrawCard()
+	if err != nil {
+		return err
+	}
+	hand.addCard(card)
+	return nil
+}
+
+func (round *Round) GetDealerHand() *Hand {
+	return round.dealerHand
+}
+
+func (round *Round) GetPlayerHand() *Hand {
+	return round.playerHand
+}
