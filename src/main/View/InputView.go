bcc615b6860edb1d1aff72291fa653adc6df6435
patkrdj <taejun154@gmail.com>
2025-11-14 11:14:30 +0900

    feat(InputView.go): 히트할지 스탠드할지 옵션을 입력받는 코드 구현

--- src/main/View/InputView.go
+++ src/main/View/InputView.go
@@ -33,4 +33,19 @@ func (view *InputView) ReadBet() (int, error) {
 	return num, nil
 }
 
-func (view *InputView) ReadInt() (int, error) {}
+func (view *InputView) ReadOption() (int, error) {
+	fmt.Print("1. Hit\n2. Stand\n")
+	input, err := view.Reader.ReadString('\n')
+	if err != nil {
+		return -1, fmt.Errorf("입력 읽기 실패")
+	}
+	input = strings.TrimSpace(input)
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		return -1, fmt.Errorf("정수 변환 실패")
+	}
+	if num != 1 && num != 2 {
+		return -1, fmt.Errorf("1번과 2번 중에 선택해야 함")
+	}
+	return num, nil
+}
