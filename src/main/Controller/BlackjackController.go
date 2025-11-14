2cff42686b36373b80e0d9a20ee89476d3ee8ef0
patkrdj <taejun154@gmail.com>
2025-11-14 11:32:27 +0900

    feat(BlackjackController.go): Model과 View를 사용하여 게임 흐름에 따라 호출

--- src/main/Controller/BlackjackController.go
+++ src/main/Controller/BlackjackController.go
@@ -5,14 +5,59 @@ import (
 	"Blackjack/src/main/View"
 )
 
-func run() error {
+func Run() error {
 	inputView := View.NewInputView()
+	outputView := View.NewOutputView()
+
 	bet, err := inputView.ReadBet()
 	if err != nil {
 		return err
 	}
 
 	round := Model.NewRound(Model.NewDeck(nil), bet)
+	err = round.DealInit()
+	if err != nil {
+		return err
+	}
 
-	inputView.Read
+	err = outputView.PrintDealerCard(*round)
+	if err != nil {
+		return err
+	}
+
+	err = outputView.PrintPlayerCards(*round)
+	if err != nil {
+		return err
+	}
+
+	for !round.GetPlayerHand().IsBurst() {
+		opt, err := inputView.ReadOption()
+		if err != nil {
+			return err
+		}
+
+		if opt == 1 {
+			err = round.PlayerHit()
+			if err != nil {
+				return err
+			}
+			err = outputView.PrintDealerCard(*round)
+			if err != nil {
+				return err
+			}
+			err = outputView.PrintPlayerCards(*round)
+			if err != nil {
+				return err
+			}
+		} else {
+			break
+		}
+	}
+
+	err = round.DealerTurn()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
