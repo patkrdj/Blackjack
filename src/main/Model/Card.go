8db8eb59ae3403ff4dfe62e30203665af00cfeec
patkrdj <taejun154@gmail.com>
2025-11-14 10:18:10 +0900

    feat(Card.go): get method 추가

--- src/main/Model/Card.go
+++ src/main/Model/Card.go
@@ -8,3 +8,11 @@ type Card struct {
 func NewCard(suit string, value string) Card {
 	return Card{suit: suit, value: value}
 }
+
+func (card *Card) GetSuit() string {
+	return card.suit
+}
+
+func (card *Card) GetValue() string {
+	return card.value
+}
